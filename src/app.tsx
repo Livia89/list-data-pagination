a2c9b16538bab5ec5a6193df864132ccec0cce63
Livia89 <liviapriscilla1989@gmail.com>
2024-02-21 21:49:24 +0000

    feat: react router dom added

--- src/app.tsx
+++ src/app.tsx
@@ -12,7 +12,52 @@ import {
 	TableRow,
 } from './components/ui/table';
 import { Pagination } from './components/pagination';
+import { keepPreviousData, useQuery } from '@tanstack/react-query';
+import { useSearchParams } from 'react-router-dom';
+import { useState } from 'react';
+
+export interface TagResponse {
+	first: number;
+	prev: number | null;
+	next: number;
+	last: number;
+	pages: number;
+	items: number;
+	data: Daum[];
+}
+
+export interface Daum {
+	title: string;
+	amountOfVideos: number;
+	id: string;
+}
+
 function App() {
+	const [searchParams] = useSearchParams();
+	const [filter, setFilter] = useState('');
+
+	const page = searchParams.get('page') ? Number(searchParams.get('page')) : 1;
+
+	// manipulate data from API server
+	const { data: tagsResponse, isLoading } = useQuery<TagResponse>({
+		queryKey: ['get-tags', page], // each page is saved in cache
+		queryFn: async () => {
+			const response = await fetch(
+				`http://localhost:3333/tags?_page=${page}&_per_page=10`
+			);
+			const data = response.json();
+
+			// delay 2s
+			await new Promise(resolve => setTimeout(resolve, 2000));
+			return data;
+		},
+		placeholderData: keepPreviousData, // show data after loading
+		staleTime: 1000 * 60, // clear cache in 60s
+	});
+
+	if (isLoading) {
+		return null;
+	}
 	return (
 		<div className="py-10 space-y-8">
 			<div>
@@ -30,7 +75,11 @@ function App() {
 				<div className="flex items-center justify-between">
 					<Input variant="filter">
 						<Search className="size-3" />
-						<Control placeholder="Search tags..." />
+						<Control
+							placeholder="Search tags..."
+							onChange={e => setFilter(e.target.value)}
+							value={filter}
+						/>
 					</Input>
 					<div className=""></div>
 					<Button>
@@ -47,19 +96,19 @@ function App() {
 						</TableRow>
 					</TableHeader>
 					<TableBody>
-						{Array.from({ length: 10 }).map((value, index) => {
+						{tagsResponse?.data.map(tag => {
 							return (
-								<TableRow key={index}>
+								<TableRow key={tag.id}>
 									<TableCell></TableCell>
 									<TableCell>
 										<div className="flex flex-col gap-0.5">
-											<span className="font-medium">React</span>
-											<span className="text-xs text-zinc-500">
-												c3057d87-243d-4b08-8a41-03bd3d783dae
-											</span>
+											<span className="font-medium">{tag.title}</span>
+											<span className="text-xs text-zinc-500">{tag.id}</span>
 										</div>
 									</TableCell>
-									<TableCell className="text-zinc-300">13 Video(s)</TableCell>
+									<TableCell className="text-zinc-300">
+										{tag.amountOfVideos} Video(s)
+									</TableCell>
 									<TableCell className="text-right">
 										<Button size="icon">
 											<MoreHorizontal className="size-4" />
@@ -70,7 +119,13 @@ function App() {
 						})}
 					</TableBody>
 				</Table>
-				<Pagination items={0} page={0} pages={0} />
+				{tagsResponse && (
+					<Pagination
+						items={tagsResponse.items}
+						page={page}
+						pages={tagsResponse.pages}
+					/>
+				)}
 			</main>
 		</div>
 	);
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,23 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './app.tsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import App from './app.tsx';
+import './index.css';
+
+const queryClient = new QueryClient();
+
+const router = createBrowserRouter([
+	{
+		path: '/',
+		element: <App />,
+	},
+]);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+	<React.StrictMode>
+		<QueryClientProvider client={queryClient}>
+			<RouterProvider router={router} />
+		</QueryClientProvider>
+	</React.StrictMode>
+);
